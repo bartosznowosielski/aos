a745ecaa0452c5e1fd92776dcbde92a5d67703f6
Bartek Nowosielski <nowosielski.bartosz@gmail.com>
2021-01-26 13:25:51 +0100

    add aos

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
 import '../scss/main.scss';
+import AOS from 'aos';
+import '../scss/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init({
+    duration: 1000
+  });
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
